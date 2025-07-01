f7ccfd06c3860d578b9c753d2d6ae037786f218f
Margaux Flores <margaux@tailor.tech>
2025-07-01 09:32:36 +0900

    feat: integrate tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
@@ -23,4 +24,7 @@ export default defineConfig({
 			],
 		}),
 	],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
